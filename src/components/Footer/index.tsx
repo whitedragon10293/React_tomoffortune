7c662ffd31cf1a16fc471aef27475b5f15dd7ab3
luckyguy <lucky1024guy@gmail.com>
2023-05-05 13:12:38 +0300

    Update mobile responsive on map page

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,11 +4,11 @@ const Footer = () => {
   return (
     <footer className="container flex justify-end w-full absolute bottom-0 p-4 z-10">
       <button
-        className="bg-twitter-btn-bg hover:bg-twitter-btn-bg-hover bg-contain w-16 h-16"
+        className="bg-twitter-btn-bg hover:bg-twitter-btn-bg-hover bg-contain bg-no-repeat w-16 h-16"
         onClick={() => window.open("https://twitter.com/TombsOfFortune")}
       />
       <button
-        className="bg-discord-btn-bg hover:bg-discord-btn-bg-hover bg-contain w-16 h-16 ml-5"
+        className="bg-discord-btn-bg hover:bg-discord-btn-bg-hover bg-contain bg-no-repeat w-16 h-16 ml-5"
         onClick={() => window.open("https://discord.com/invite/Rg4QkDUwcJ")}
       />
     </footer>
--- src/components/Header/partials/ConnectWalletButton/index.tsx
+++ src/components/Header/partials/ConnectWalletButton/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ConnectWalletButton = () => {
   return (
-    <button className="bg-connectWallet-btn-bg hover:bg-connectWallet-btn-bg-hover bg-contain rounded-md w-60 h-16 text-color-2 text-center text-lg uppercase pt-6">
+    <button className="bg-connectWallet-btn-bg hover:bg-connectWallet-btn-bg-hover bg-contain bg-center bg-no-repeat rounded-md w-48 lg:w-60 h-16 text-color-2 text-center text-sm lg:text-lg uppercase pt-4 lg:pt-4">
       Connect your wallet
     </button>
   );
