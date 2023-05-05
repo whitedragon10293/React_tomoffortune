38441dbadc9413d074a657aa6572b430ca7ffbdf
luckyguy <lucky1024guy@gmail.com>
2023-05-05 12:19:05 +0300

    Update Reception page to go to page when clicking buttons

--- src/pages/Reception/index.tsx
+++ src/pages/Reception/index.tsx
@@ -3,7 +3,6 @@ import ImageButton from "./partials/ImageButton";
 import Background from "assets/Reception/background.png";
 
 const Reception = () => {
-  const handleClick = () => {};
   return (
     <>
       <img
@@ -13,8 +12,8 @@ const Reception = () => {
       ></img>
       <div className="container w-full h-full flex justify-center items-center relative">
         <div className="relative flex flex-col md:flex-row justify-center items-center mt-72 w-full">
-          <ImageButton content="play" onClick={handleClick}></ImageButton>
-          <ImageButton content="mint nft" onClick={handleClick}></ImageButton>
+          <ImageButton content="play" url="/map"></ImageButton>
+          <ImageButton content="mint nft" url="/mint"></ImageButton>
         </div>
       </div>
     </>
--- src/pages/Reception/partials/ImageButton/index.tsx
+++ src/pages/Reception/partials/ImageButton/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 interface props {
   content: string;
-  onClick: () => void;
+  url: string;
 }
-const ImageButton: React.FC<props> = ({ content, onClick }) => {
+const ImageButton: React.FC<props> = ({ content, url }) => {
+  const navigate = useNavigate();
   return (
     <button
       className="bg-color-1 bg-reception-btn-bg w-80 h-24 rounded-md text-color-2 hover:text-color-3 text-center text-5xl uppercase m-4"
-      onClick={onClick}
+      onClick={() => navigate(url)}
       style={{ backgroundSize: "100% 100%" }}
     >
       {content}
