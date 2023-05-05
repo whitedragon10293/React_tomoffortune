b91a25d49f57a8642e64412e000e47f96be3fce8
luckyguy <lucky1024guy@gmail.com>
2023-05-05 14:30:22 +0300

    Update mobile resonsive on Reception page

--- src/pages/Reception/partials/ImageButton/index.tsx
+++ src/pages/Reception/partials/ImageButton/index.tsx
@@ -9,7 +9,7 @@ const ImageButton: React.FC<props> = ({ content, url }) => {
   const navigate = useNavigate();
   return (
     <button
-      className="bg-color-1 bg-reception-btn-bg w-80 h-24 rounded-md text-color-2 hover:text-color-3 text-center text-5xl uppercase m-4"
+      className="bg-color-1 bg-reception-btn-bg w-80 h-20 rounded-md text-color-2 hover:text-color-3 text-center text-2xl md:text-5xl uppercase m-4"
       onClick={() => navigate(url)}
       style={{ backgroundSize: "100% 100%" }}
     >
